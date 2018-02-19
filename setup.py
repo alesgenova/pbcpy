23a209f6d644900e3c04352829e34a9cdad07633
Alessandro Genova <ales.genova@gmail.com>
2018-02-18 23:12:47 -0500

    release a new version to PyPI

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(name='pbcpy',
-      description='''A toolbox to make it easier to deal with materials under
-                    periodc boundary conditions.''',
+      description='''A toolbox to make it easier to deal with materials under periodc boundary conditions.''',
       long_description='''pbcpy is a Python3 package providing some useful tools when dealing with
                           molecules and materials under periodic boundary conditions (PBC).
                           Foundation of the package are the Cell and Coord classes, which define a unit cell under PBC, and a cartesian/crystal coordinate respectively.
-                          pbcpy also provides tools to deal with quantities represented on an equally spaced grids, through the Grid and Plot classes. Operations such as interpolations or taking arbitrary 1D/2D/3D cuts are made very easy.
+                          pbcpy also provides tools to deal with quantities represented on an equally spaced grids, through the Grid and Field classes. Operations such as interpolations or taking arbitrary 1D/2D/3D cuts are made very easy.
                           In addition, pbcpy exposes a fully periodic N-rank array, the pbcarray, which is derived from the numpy.ndarray.
                           Finally, pbcpy provides IO support to some common file formats:
                             The Quantum Espresso .pp format (read only)
                             The XCrySDen .xsf format (write only) ''',
-      version='17.02',
+      version='0.2.1',
       url='https://gitlab.com/ales.genova/pbcpy/',
       author='A. Genova',
       author_email='ales.genova@gmail.com',
